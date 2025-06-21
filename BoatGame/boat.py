14082641a6412dece2f1a0aa5524be4ea6ba08cc
jbeh444 <jaredbeh444@gamil.com>
2025-06-20 18:02:51 -0600

    updates

--- BoatGame/boat.py
+++ BoatGame/boat.py
@@ -1,9 +1,15 @@
 import pgzrun
 from random import randint
 
+# ----------------------------
+# Window Settings
+# ----------------------------
 WIDTH = 1000
 HEIGHT = 600
 
+# ----------------------------
+# Shop Item Definitions
+# ----------------------------
 shop_items = {
     "wood": {"price": 50},
     "stone": {"price": 100},
@@ -11,51 +17,82 @@ shop_items = {
     "flag": {"price": 1000}
 }
 
-shop = Actor("shop")
+# ----------------------------
+# Actor Initialization
+# ----------------------------
+# UI Elements
+shop = Actor("shop")       # type: ignore  # noqa: F821
 shop.pos = 900, 300
-sail = Actor("sail")
+
+sail = Actor("sail")       # type: ignore  # noqa: F821
 sail.pos = 900, 200
-rock = Actor("rock")
-rock.pos = 1600, randint(350, 550)
-play = Actor("play")
+
+play = Actor("play")       # type: ignore  # noqa: F821
 play.pos = 500, 257
-metalblock = Actor("metalblock")
-metalblock.pos = 210, 250
-stoneblock = Actor("stoneblock")
+
+exit = Actor("exit")       # type: ignore  # noqa: F821
+exit.pos = 500, 200
+
+# Blocks
+woodblock = Actor("woodblock")       # type: ignore  # noqa: F821
+woodblock.pos = 90, 250
+
+stoneblock = Actor("stoneblock")     # type: ignore  # noqa: F821
 stoneblock.pos = 150, 250
-woodblock = Actor("woodblock")
-woodblock.pos = 90, 250 
-treasurechest = Actor("tresurechest")
-treasurechest.pos = 1600, 400
-flag = Actor("flag")
+
+metalblock = Actor("metalblock")     # type: ignore  # noqa: F821
+metalblock.pos = 210, 250
+
+flag = Actor("flag")       # type: ignore  # noqa: F821
 flag.pos = 270, 250
 
+# Game Objects
+rock = Actor("rock")       # type: ignore  # noqa: F821
+rock.pos = 1600, randint(350, 550)
+
+treasurechest = Actor("tresurechest")  # type: ignore  # noqa: F821
+treasurechest.pos = 1600, 400
+
+# ----------------------------
+# Game State Variables
+# ----------------------------
 sail_clicked_fun = False
 music_playing = False
 flagplaced = False
 playing = False
-number_of_rocks = 99
+shop_open = False
+# ----------------------------
+# Inventory & Gameplay Stats
+# ----------------------------
+number_of_rocks = 0 #set to zero whenrunning the game for the first time
 money = 0
 woodcount = 5
 stoneblockcount = 2
 metalblockcount = 1
 flagcount = 1
+missed_rocks = 0
+
+# ----------------------------
+# Gameplay Mechanics
+# ----------------------------
 selected_block = None
 placed_blocks = []
 
+
 def draw():
     global sail_clicked_fun
-    screen.blit("ocean", (0, 0))
+    screen.blit("ocean", (0, 0)) # type: ignore  # noqa: F821
     if playing:
         if sail_clicked_fun and music_playing: 
             if number_of_rocks >= 100:
                 treasurechest.draw()      
             else:
                 rock.draw()
+                exit.draw()
             for block in placed_blocks:
                 block["actor"].draw()
 
-            screen.draw.text("Money: " + str(money), (500, 10), color="black")
+            screen.draw.text("Money: " + str(money), (500, 10), color="black") # type: ignore  # noqa: F821
         else:           
             #Drawing lines (Start position->(x.pos, y.pos), Stop position->(x.pos, y.pos), color)
             #Horizontal
@@ -66,31 +103,36 @@ def draw():
             for block in placed_blocks:
                 block["actor"].draw()
 
-            screen.draw.text("Money: " + str(money), (850, 520), color="black")
+            screen.draw.text("Money: " + str(money), (850, 520), color="black") # type: ignore  # noqa: F821
                      
             # Draw horizontal grid lines
             for y in range(286, 600, 60):
-                screen.draw.line((0, y), (600, y), color="black")
+                screen.draw.line((0, y), (600, y), color="black") # type: ignore  # noqa: F821
 
             # Draw vertical grid lines
             for x in range(60, 601, 60):
-                screen.draw.line((x, 286), (x, 586), color="black")
+                screen.draw.line((x, 286), (x, 586), color="black") # type: ignore  # noqa: F821
 
             # Build menu horizontal lines
-            screen.draw.line((60, 220), (300, 220), color="black")
-            screen.draw.line((60, 280), (300, 280), color="black")
+            screen.draw.line((60, 220), (300, 220), color="black") # type: ignore  # noqa: F821
+            screen.draw.line((60, 280), (300, 280), color="black") # type: ignore  # noqa: F821
 
             # Build menu vertical dividers
             for x in range(60, 301, 60):
-                screen.draw.line((x, 220), (x, 280), color="black")
+                screen.draw.line((x, 220), (x, 280), color="black") # type: ignore  # noqa: F821
             
-            # Draw block counts above the menu
-            # Better-aligned block counts above the menu
-            screen.draw.text("Wood: " + str(woodcount), (60, 200), color="black", fontsize=20)
-            screen.draw.text("Stone: " + str(stoneblockcount), (125, 200), color="black", fontsize=20)
-            screen.draw.text("Metal: " + str(metalblockcount), (190, 200), color="black", fontsize=20)
-            screen.draw.text("Flag: " + str(flagcount), (255, 200), color="black", fontsize=20)
+           # Prices
+            screen.draw.text("Press shop to buy selected block", (60, 160), color="black", fontsize=18) # type: ignore  # noqa: F821
+            screen.draw.text("50$", (70, 180), color="black", fontsize=18) # type: ignore  # noqa: F821
+            screen.draw.text("100$", (130, 180), color="black", fontsize=18) # type: ignore  # noqa: F821
+            screen.draw.text("500$", (190, 180), color="black", fontsize=18) # type: ignore  # noqa: F821
+            screen.draw.text("1000$", (255, 180), color="black", fontsize=18) # type: ignore  # noqa: F821
 
+            # Counts
+            screen.draw.text("Wood: " + str(woodcount), (60, 200), color="black", fontsize=20) # type: ignore  # noqa: F821
+            screen.draw.text("Stone: " + str(stoneblockcount), (125, 200), color="black", fontsize=20) # type: ignore  # noqa: F821
+            screen.draw.text("Metal: " + str(metalblockcount), (190, 200), color="black", fontsize=20) # type: ignore  # noqa: F821
+            screen.draw.text("Flag: " + str(flagcount), (255, 200), color="black", fontsize=20) # type: ignore  # noqa: F821
 
             
             sail.draw()
@@ -100,8 +142,10 @@ def draw():
     
 def on_mouse_down(pos, button):
     global playing, selected_block
+    if exit.collidepoint(pos):
+        restart_game()
     if not playing or not sail_clicked_fun:  # 🔒 Only allow input before sailing
-        if button == mouse.LEFT:
+        if button == mouse.LEFT: # type: ignore  # noqa: F821
             if sail.collidepoint(pos):
                 sail_clicked()
             elif shop.collidepoint(pos):
@@ -109,26 +153,26 @@ def on_mouse_down(pos, button):
             elif play.collidepoint(pos) and not sail_clicked_fun:
                 playing = True
             elif woodblock.collidepoint(pos):
-                selected_block = "wood"
+                selected_block = "wood"                
             elif stoneblock.collidepoint(pos):
                 selected_block = "stone"
             elif metalblock.collidepoint(pos):
                 selected_block = "metal"
             elif flag.collidepoint(pos):
-                selected_block = "flag"
+                selected_block = "flag"            
             else:
                 place_block(pos)
-        elif button == mouse.RIGHT:
+        elif button == mouse.RIGHT: # type: ignore  # noqa: F821
             delete_block(pos)
 
 def open_shop_menu():
-    global money, woodcount, stoneblockcount, metalblockcount, flagcount
+    global money, woodcount, stoneblockcount, metalblockcount, flagcount   
     if selected_block and selected_block in shop_items:
         cost = shop_items[selected_block]["price"]
         if money >= cost:
             money -= cost
             if selected_block == "wood":
-                woodcount += 1
+                woodcount += 1                
             elif selected_block == "stone":
                 stoneblockcount += 1
             elif selected_block == "metal":
@@ -147,14 +191,14 @@ def sail_clicked():
 
     sail_clicked_fun = True
     if not music_playing:
-        music.play("vanishing-horizon")
+        music.play("vanishing-horizon") # type: ignore  # noqa: F821
         music_playing = True
 
         
         
 def update():
     global rock, number_of_rocks, treasurechest, sail_clicked_fun, money, music_playing, sail_clicked_fun
-    global placed_blocks, flagplaced, flagcount, woodcount, stoneblockcount, metalblockcount
+    global placed_blocks, flagplaced, flagcount, woodcount, stoneblockcount, metalblockcount, missed_rocks
     if sail_clicked_fun:
         if rock.right > 0:        
             rock.x -= 5
@@ -176,13 +220,24 @@ def update():
                     rock.x = 1600
                     rock.y = randint(350, 550)
                     number_of_rocks += 1
+                    missed_rocks = 0
                     money += 8
                     break
         else:
             # Rock missed everything, still reset!
             rock.x = 1600
-            rock.y = randint(350, 550)
+
+            if missed_rocks >= 2:
+                # Try upper and lower positions alternately
+                if missed_rocks == 2:
+                    rock.y = 350  # Upper range
+                else:
+                    rock.y = 550  # Lower range
+            else:
+                rock.y = randint(350, 550)  # Default range
+
             number_of_rocks += 1
+            missed_rocks += 1
             money += 8
 
          # Check if the flag is broken
@@ -222,16 +277,16 @@ def place_block(pos):
 
             new_block = None
             if selected_block == "wood" and woodcount > 0:
-                new_block = Actor("woodblock")
+                new_block = Actor("woodblock") # type: ignore  # noqa: F821
                 woodcount -= 1
             elif selected_block == "stone" and stoneblockcount > 0:
-                new_block = Actor("stoneblock")
+                new_block = Actor("stoneblock") # type: ignore  # noqa: F821
                 stoneblockcount -= 1
             elif selected_block == "metal" and metalblockcount > 0:
-                new_block = Actor("metalblock")
+                new_block = Actor("metalblock") # type: ignore  # noqa: F821
                 metalblockcount -= 1
             elif selected_block == "flag" and flagcount > 0:
-                new_block = Actor("flag")
+                new_block = Actor("flag") # type: ignore  # noqa: F821
                 flagcount -= 1
 
             if new_block:
@@ -268,13 +323,16 @@ def delete_block(pos):
                 flagcount += 1
             placed_blocks.remove(block)
             break
+        
 def restart_game():
     global number_of_rocks, money, sail_clicked_fun, music_playing, game_over
-    global rock, treasurechest, placed_blocks
+    global rock, treasurechest, placed_blocks, missed_rocks
     number_of_rocks = 0    
     sail_clicked_fun = False
-    music_playing = False       
-    music.stop()
+    music_playing = False    
+    missed_rocks = 0
+    rock.x = 1600 # Reset rock position   
+    music.stop() # type: ignore  # noqa: F821
     # Optionally reset block counts too:
     
 
